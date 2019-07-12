e2e3a5359bb1dac258db1c5a502320fb723cea28
domsangio <domenic_sangiovanni@yahoo.com>
2019-07-11 20:02:01 -0400

    Looks bad but works

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import 'Footer.css'
+import './Footer.css'
 
 export const Footer = () => (
-    <div>
-        <div className='footer name'>Domenic SanGiovanni</div>
-        <div className='footer extra'>domsangio@gmail.com</div>
-        <div className='footer extra'><a href='github.com/domsangio'>github.com/domsangio</a></div>
+    <div className='footer'>
+        <div className='name'>Domenic SanGiovanni</div>
+        <div className='extra'>domsangio@gmail.com</div>
+        <div className='extra'><a href='github.com/domsangio'>github.com/domsangio</a></div>
     </div>
 )
