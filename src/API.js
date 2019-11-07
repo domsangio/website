a9372bd1ec4c6b8d342353f93f38149343e0f04c
domsangio <domenic_sangiovanni@yahoo.com>
2019-11-06 23:11:49 -0500

    worked on it a little

--- src/API.js
+++ src/API.js
@@ -13,14 +13,12 @@ export const getProjectListAll = () => {
 
 /* this needs to be fixed obv TODO */
 export const getAPOD = () => {
-    return fetch('http://127.0.0.1:5000/projects/list?project=all', 
+    return fetch(`https://api.nasa.gov/planetary/apod?api_key=NCTrauYGs1Q00lP5zPZosbRf4HxuhrMxhIP6lcdW`, 
     {
         method: 'GET',
         header: { accessControlAllowOrigin: '*' }
     }
-    ).then( data => {
+    ).then(data => {
         return data.json()    
-    }).then(data => {
-        console.log(data)
     })
 }
--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Header } from '../components/Header/Header'
 import { Footer } from '../components/Footer/Footer'
 import { AboutInformation } from '../components/AboutInformation/AboutInformation'
+import { getAPOD } from '../API'
 
 export function AboutContainer() {
     return (
-        <div>
+        <div backgroundImage = {getAPOD()['url']} >
             <Header />
             <AboutInformation />
             <Footer />
